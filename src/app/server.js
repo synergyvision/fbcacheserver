c41fa723d366e13ec7afe96d88beb5398cbdc143
LuisRFuentes <luisrf0540@gmail.com>
2020-09-06 23:01:59 -0400

    Cambios varios en el servidor -Agrega varias variables de entorno -Agrega archivo de configuración de FBCache

--- src/app/server.js
+++ src/app/server.js
@@ -5,7 +5,20 @@ import { logger } from "../utils/logger";
 import { FBCache } from "fbcache";
 import * as fbcConfig from "../../fbcache.config.json";
 
-FBCache.init(fbcConfig);
+const firebaseCredential = {
+    type: config.CREDENTIAL_FILE_TYPE,
+    project_id: config.CREDENTIAL_FILE_PROYECT_ID,
+    private_key_id: config.CREDENTIAL_FILE_PRIVATE_KEY_ID,
+    private_key: config.CREDENTIAL_FILE_PRIVATE_KEY,
+    client_email: config.CREDENTIAL_FILE_CLIENT_EMAIL,
+    client_id: config.CREDENTIAL_FILE_CLIENT_ID,
+    auth_uri: config.CREDENTIAL_FILE_AUTH_URI,
+    token_uri: config.CREDENTIAL_FILE_TOKEN_URI,
+    auth_provider_x509_cert_url: config.CREDENTIAL_FILE_AUTH_PROV_CERT,
+    client_x509_cert_url: config.CREDENTIAL_FILE_CLIENT_CERT
+};
+
+FBCache.init(fbcConfig, config.URL, config.CREDENTIAL_TYPE, firebaseCredential);
 export const app = express();
 
 // MIDDLEWARES
@@ -14,6 +27,11 @@ app.use(express.json());
 
 // SETTINGS
 app.set('port', config.PORT || 3000);
+
+// ROUTES
+app.use('/api', (req, res) => {
+    res.json("FBCache Server is running")
+});
 
 // ERROR HANDLER
 app.use(function(err, req, res, next) {
